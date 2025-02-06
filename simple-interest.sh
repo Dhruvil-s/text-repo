ac8715669888b3a90ec6701ab1d83ef42c468d9d
Dhruvil-s <gondaliyadhruvil2005@gmail.com>
2025-02-06 17:39:31 +0530

    Create  simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,7 @@
+The Simple Interest Calculator is a basic shell script that takes input from the user for:
+
+Principal amount
+Rate of interest
+Time period (in years)
+It calculates the simple interest using the formula:
+Simple Interest = (Principal × Rate × Time) / 100
